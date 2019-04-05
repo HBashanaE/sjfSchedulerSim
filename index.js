8f6ebc846ca7890311d88b860edb7e4c4e899286
Bashana Elikewela <hbashanae@gmail.com>
2019-04-05 19:28:55 +0530

    Main changes

--- index.js
+++ index.js
@@ -1,57 +1,48 @@
-var processCount = 0;
-var processList = [];
 $(function () {
-    $("#createProcess").click(
+    $("#addProcess").click(
         function () {
-            var textInput = $("#burstTime").val();
-            if($.isNumeric(textInput)){
-                if(textInput!=''){
-                    var processName= "Process"+processCount;
-                    $('#processList0').append("<li class='text-white'>" +processName+ "</li>");
-                    $('#burstTimeList0').append("<li class='text-white'>"+textInput +"</li>");
-                    processCount++;
-                    $("#burstTime").val(""); //or$(this).value = '';
+            var burstTimeInput = $("#burstTime").val();
+            var arivalTimeInput = $("#arivalTime").val();
+            if($.isNumeric(burstTimeInput) && $.isNumeric(arivalTimeInput)){
+                if(burstTimeInput!='' && arivalTimeInput!= ''){
 
-                    $('#ganttChart0').append("<div style='width:"+textInput+"px;height:100px;border:1px solid #000; color:#f15468'></div>")
+                    var processItemList = [];
+                    var processList0 = [];
+                    var processList1 = [];
 
-                    //processList.push(parseInt(textInput));
-                    var processItem = {};
-                    processItem[processName] = textInput;
-                    // $.each(processItem,function(index,value){
-                    //     console.log(index,value);
-                    // });
-                    processList.push(processItem);
+                    $("#burstTime").val("");
+                    $("#arivalTime").val("");
 
-                    inc = 0;
-                    $.each(processList,function(index,value){
-                        var processValue = value["Process"+inc];
-                        // console.log(processValue);
-                        $.each(processList[index],function(ind,val){
-                            //console.log(val);
-                        });
-                        //console.log(value);
-                        inc++;
-                    });
-                    processList.sort(function (a, b) {  
+                    var processItem0 = {};
+                    processItem0[arivalTimeInput] = burstTimeInput;
+                    processItemList.push(processItem0);
+
+                    processItemList.sort(function (a, b) {  
                         $.each(a,function(x,y){
-                            valA= y;
+                            valA= x;
                         });
                         $.each(b,function(x,y){
-                            valB = y;
+                            valB = x;
                         });
                         return valA-valB;  });
-
-                        inc = 0;
-                        $.each(processList,function(index,value){
-                            var processValue = value["Process"+inc];
-                            // console.log(processValue);
-                            $.each(processList[index],function(ind,val){
-                                console.log(ind);
-                            });
-                            //console.log(value);
-                            inc++;
-                        });
                     
+                    
+                    var inc = 0;
+                    $.each(processItemList,function(index,value){
+                        var processItem1 = [];
+                        $.each(processItemList[index],function(ind,val){
+                            processName = "Process"+inc;
+                            processItem1.push(processName);
+                            console.log(processName);
+                            processItem1.push(ind);
+                            console.log(ind);
+                            processItem1.push(val);
+                            console.log(val);
+                        });
+                        processList0.push(processItem1);
+                        console.log(processList0);
+                        inc++;
+                    });
 
                     $('#ganttChart1').empty();
                     $('#processList1').empty();
@@ -60,14 +51,37 @@ $(function () {
                     $('#burstTimeList1').append("<div class='text-white'> <strong>Burst time(ms)</strong> <br></div>");
                     
                     inc = 0;
-                    $.each(processList,function(index,value){
-                        $.each(processList[index],function(ind,val){
-                            $('#processList1').append("<li class='text-white'>" +ind+ "</li>");
-                            $('#burstTimeList1').append("<li class='text-white'>"+val+"</li>");
-                            $('#ganttChart1').append("<div style='width:"+val+"px;height:100px;border:1px solid #000; color:#f15468'></div>");
-                        });
+                    $.each(processList0, function (index, value) {
+                            $('#processList0').append("<li class='text-white'>" + value[0] + "</li>");
+                            $('#arivalTimeList0').append("<li class='text-white'>" + value[1]+ "</li>");
+                            $('#burstTimeList0').append("<li class='text-white'>" + value[2] + "</li>");
+                            $('#ganttChart1').append("<div style='width:" + value[2] + "px;height:100px;border:1px solid #000; color:#f15468'></div>");
                         inc++;
                     });
+
+
+                    // inc = 0;
+                    // $.each(processList,function(index,value){
+                    //     var processValue = value["Process"+inc];
+                    //     // console.log(processValue);
+                    //     $.each(processList[index],function(ind,val){
+                    //         console.log(ind);
+                    //     });
+                    //     //console.log(value);
+                    //     inc++;
+                    // });
+                    
+
+                    
+                    // inc = 0;
+                    // $.each(processItemList,function(index,value){
+                    //     $.each(processItemList[index],function(ind,val){
+                    //         $('#processList1').append("<li class='text-white'>" +ind+ "</li>");
+                    //         $('#burstTimeList1').append("<li class='text-white'>"+val+"</li>");
+                    //         $('#ganttChart1').append("<div style='width:"+val+"px;height:100px;border:1px solid #000; color:#f15468'></div>");
+                    //     });
+                    //     inc++;
+                    // });
 
                 }else{
                     alert("Add a burst time");
