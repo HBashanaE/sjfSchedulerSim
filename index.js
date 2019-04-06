6c8bfcfb8e129484784677c7477a7e267ab12b0e
Bashana Elikewela <hbashanae@gmail.com>
2019-04-06 17:51:08 +0530

    With arival time

--- index.js
+++ index.js
@@ -1,92 +1,90 @@
+
 $(function () {
+    var processItemList = [];
+    var processList1 = [];
     $("#addProcess").click(
         function () {
+            var processList0 = [];
             var burstTimeInput = $("#burstTime").val();
             var arivalTimeInput = $("#arivalTime").val();
             if($.isNumeric(burstTimeInput) && $.isNumeric(arivalTimeInput)){
                 if(burstTimeInput!='' && arivalTimeInput!= ''){
 
-                    var processItemList = [];
-                    var processList0 = [];
-                    var processList1 = [];
-
                     $("#burstTime").val("");
                     $("#arivalTime").val("");
-
-                    var processItem0 = {};
-                    processItem0[arivalTimeInput] = burstTimeInput;
+ 
+                    var processItem0 = [];
+                    processItem0.push(arivalTimeInput);
+                    processItem0.push(burstTimeInput);
                     processItemList.push(processItem0);
-
+                    
                     processItemList.sort(function (a, b) {  
-                        $.each(a,function(x,y){
-                            valA= x;
-                        });
-                        $.each(b,function(x,y){
-                            valB = x;
-                        });
-                        return valA-valB;  });
-                    
-                    
-                    var inc = 0;
-                    $.each(processItemList,function(index,value){
-                        var processItem1 = [];
-                        $.each(processItemList[index],function(ind,val){
-                            processName = "Process"+inc;
-                            processItem1.push(processName);
-                            console.log(processName);
-                            processItem1.push(ind);
-                            console.log(ind);
-                            processItem1.push(val);
-                            console.log(val);
-                        });
-                        processList0.push(processItem1);
-                        console.log(processList0);
-                        inc++;
+                        var valA = a[0];
+                        var valB = b[0];
+                        return valA-valB;  
                     });
 
-                    $('#ganttChart1').empty();
-                    $('#processList1').empty();
-                    $('#processList1').append("<div class='text-white'> <strong>Process Name </strong><br></div>");
-                    $('#burstTimeList1').empty();
-                    $('#burstTimeList1').append("<div class='text-white'> <strong>Burst time(ms)</strong> <br></div>");
+                    var inc = 0;
+                    $.each(processItemList,function(a,b){
+                        processName = "Process"+inc;
+                        var processItem1 = [];
+                        processItem1.push(processName);
+                        processItem1.push(b[0]);
+                        processItem1.push(b[1]);
+                        processList0.push(processItem1);
+                        inc++;
+                    });               
+
+                    $('#ganttChart0').empty();
+                    $('#processList0').empty();
+                    $('#processList0').append("<div class='text-white'> <strong>Process Name </strong><br></div>");
+                    $('#arivalTimeList0').empty();
+                    $('#arivalTimeList0').append("<div class='text-white'> <strong>Arival time(ms)</strong> <br></div>");
+                    $('#burstTimeList0').empty();
+                    $('#burstTimeList0').append("<div class='text-white'> <strong>Burst time(ms)</strong> <br></div>");
                     
-                    inc = 0;
                     $.each(processList0, function (index, value) {
                             $('#processList0').append("<li class='text-white'>" + value[0] + "</li>");
                             $('#arivalTimeList0').append("<li class='text-white'>" + value[1]+ "</li>");
                             $('#burstTimeList0').append("<li class='text-white'>" + value[2] + "</li>");
-                            $('#ganttChart1').append("<div style='width:" + value[2] + "px;height:100px;border:1px solid #000; color:#f15468'></div>");
-                        inc++;
+                            $('#ganttChart0').append("<div style='width:" + value[2] + "px;height:100px;border:1px solid #fff; background-color:#f15468'></div>");
                     });
 
+                    processList0.sort(function(a,b){
+                        return a[1].localeCompare(b[1]) || a[2].localeCompare(b[2]);
+                    }) 
+                    // console.log(processList0.toString());
 
-                    // inc = 0;
-                    // $.each(processList,function(index,value){
-                    //     var processValue = value["Process"+inc];
-                    //     // console.log(processValue);
-                    //     $.each(processList[index],function(ind,val){
-                    //         console.log(ind);
-                    //     });
-                    //     //console.log(value);
-                    //     inc++;
-                    // });
-                    
+                    $('#ganttChart1').empty();
+                    $('#processList1').empty();
+                    $('#processList1').append("<div class='text-white'> <strong>Process Name </strong><br></div>");
+                    $('#arivalTimeList1').empty();
+                    $('#arivalTimeList1').append("<div class='text-white'> <strong>Arival time(ms)</strong> <br></div>");
+                    $('#burstTimeList1').empty();
+                    $('#burstTimeList1').append("<div class='text-white'> <strong>Burst time(ms)</strong> <br></div>");
 
-                    
-                    // inc = 0;
-                    // $.each(processItemList,function(index,value){
-                    //     $.each(processItemList[index],function(ind,val){
-                    //         $('#processList1').append("<li class='text-white'>" +ind+ "</li>");
-                    //         $('#burstTimeList1').append("<li class='text-white'>"+val+"</li>");
-                    //         $('#ganttChart1').append("<div style='width:"+val+"px;height:100px;border:1px solid #000; color:#f15468'></div>");
-                    //     });
-                    //     inc++;
-                    // });
+                    var cumBurstTime = 0;
+                    $.each(processList0,function(index,value){
+                        cumBurstTime += value[2];
+                        currentArivalTime = value[1];
+                        
+                        // if(cumBurstTime < currentArivalTime){
+                        //     var interval = currentArivalTime - cumBurstTime;
+                        //     $('#ganttChart1').append("<div style='width:" + interval + "px;height:100px;border:1px solid #fff; background-color:#fff'></div>");
+                        // }else{
+                        //     $('#ganttChart1').append("<div style='width:" + value[2] + "px;height:100px;border:1px solid #fff; background-color:#f15468'></div>");
+                        // }
+                        $('#ganttChart1').append("<div style='width:" + value[2] + "px;height:100px;border:1px solid #fff; background-color:#f15468'></div>");
+                        $('#processList1').append("<li class='text-white'>" + value[0] + "</li>");
+                        $('#arivalTimeList1').append("<li class='text-white'>" + value[1]+ "</li>");
+                        $('#burstTimeList1').append("<li class='text-white'>" + value[2] + "</li>");
+
+                    });
 
                 }else{
                     alert("Add a burst time");
                 }
-            }else{042606
+            }else{
                 $("#burstTime").val("");
                 alert("Only numerical inputs allowed");
             }
