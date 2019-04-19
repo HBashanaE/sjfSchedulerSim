8c110025bc29f098c5ffef59610bb8e2fd7ffa8d
Bashana Elikewela <hbashanae@gmail.com>
2019-04-19 11:46:21 +0530

    Good

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 $(function () {
     var processItemList = [];
-    var processList1 = [];
     $("#addProcess").click(
         function () {
             var processList0 = [];
-            var burstTimeInput = $("#burstTime").val();
-            var arivalTimeInput = $("#arivalTime").val();
+            var processList1 = [];
+            var burstTimeInput = parseInt($("#burstTime").val());
+            var arivalTimeInput =parseInt($("#arivalTime").val());
             if($.isNumeric(burstTimeInput) && $.isNumeric(arivalTimeInput)){
                 if(burstTimeInput!='' && arivalTimeInput!= ''){
 
@@ -16,6 +16,7 @@ $(function () {
                     var processItem0 = [];
                     processItem0.push(arivalTimeInput);
                     processItem0.push(burstTimeInput);
+
                     processItemList.push(processItem0);
                     
                     processItemList.sort(function (a, b) {  
@@ -33,7 +34,7 @@ $(function () {
                         processItem1.push(b[1]);
                         processList0.push(processItem1);
                         inc++;
-                    });               
+                    });            
 
                     $('#ganttChart0').empty();
                     $('#processList0').empty();
@@ -44,16 +45,66 @@ $(function () {
                     $('#burstTimeList0').append("<div class='text-white'> <strong>Burst time(ms)</strong> <br></div>");
                     
                     $.each(processList0, function (index, value) {
-                            $('#processList0').append("<li class='text-white'>" + value[0] + "</li>");
-                            $('#arivalTimeList0').append("<li class='text-white'>" + value[1]+ "</li>");
-                            $('#burstTimeList0').append("<li class='text-white'>" + value[2] + "</li>");
-                            $('#ganttChart0').append("<div style='width:" + value[2] + "px;height:100px;border:1px solid #fff; background-color:#f15468'></div>");
+                        $('#processList0').append("<li class='text-white'>" + value[0] + "</li>");
+                        $('#arivalTimeList0').append("<li class='text-white'>" + value[1]+ "</li>");
+                        $('#burstTimeList0').append("<li class='text-white'>" + value[2] + "</li>");
+                        $('#ganttChart0').append("<div style='width:" + value[2] + "px;height:100px;border:1px solid #fff; background-color:#f15468'></div>");
                     });
 
+
+                    
                     processList0.sort(function(a,b){
-                        return a[1].localeCompare(b[1]) || a[2].localeCompare(b[2]);
-                    }) 
-                    // console.log(processList0.toString());
+                        return a[1]- b[1] || a[2] - b[2];
+                    })                     
+
+                    // arival time+burst time of first process
+                    var x = processList0[0][1] + processList0[0][2];
+                    processList1 = [];
+                    processList1.push(processList0.shift());
+
+                    //console.log(processList0);
+                    while(true){
+                        var z = 0;
+
+                        if(!processList0.length){
+                            break;
+                        }
+
+                        for(var i = 0;i< processList0.length; i++ ){
+                            if(processList0[i][1] > x){
+                                console.log('y');
+                                intermArray = processList0.splice(0,i)
+                                $.each(intermArray, function(index, value){
+                                    console.log(value); 
+                                });
+                                intermArray.sort(function(a,b){
+                                    return a[2]- b[2]
+                                });
+                                processList0.concat(intermArray);
+                                $.each(processList0, function(index, value){
+                                    console.log(value); 
+                                });
+                                z = 1;
+                                x += processList0[0][1]
+                                processList1.push(processList0.shift());
+                                break;
+                            }
+                        }
+                        
+                        if(!z){
+                            processList0.sort(function(a,b){
+                                return a[2]- b[2]
+                            });
+                            x += processList0[0][1]
+                            processList1.push(processList0.shift());
+                        }
+
+                    }
+                    //console.log(processList1); 
+                    // $.each(processList1, function(index, value){
+                    //     console.log(value); 
+                    // });
+                        
 
                     $('#ganttChart1').empty();
                     $('#processList1').empty();
@@ -63,17 +114,7 @@ $(function () {
                     $('#burstTimeList1').empty();
                     $('#burstTimeList1').append("<div class='text-white'> <strong>Burst time(ms)</strong> <br></div>");
 
-                    var cumBurstTime = 0;
-                    $.each(processList0,function(index,value){
-                        cumBurstTime += value[2];
-                        currentArivalTime = value[1];
-                        
-                        // if(cumBurstTime < currentArivalTime){
-                        //     var interval = currentArivalTime - cumBurstTime;
-                        //     $('#ganttChart1').append("<div style='width:" + interval + "px;height:100px;border:1px solid #fff; background-color:#fff'></div>");
-                        // }else{
-                        //     $('#ganttChart1').append("<div style='width:" + value[2] + "px;height:100px;border:1px solid #fff; background-color:#f15468'></div>");
-                        // }
+                    $.each(processList1,function(index,value){
                         $('#ganttChart1').append("<div style='width:" + value[2] + "px;height:100px;border:1px solid #fff; background-color:#f15468'></div>");
                         $('#processList1').append("<li class='text-white'>" + value[0] + "</li>");
                         $('#arivalTimeList1').append("<li class='text-white'>" + value[1]+ "</li>");
